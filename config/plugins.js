72c57c6ae6417d1dba2ebcc6b00b8959693352d1
G-raph <info@g-raph.be>
2021-10-30 08:55:04 +0200

    added cloudinary for images

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('dfrklekif'),
+        api_key: env('788696365145398'),
+        api_secret: env('xsctSquFILvMRh1n4NKldWAXod0'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  });
